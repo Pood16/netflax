5e64d89189cf617074103e2090d47703ab4c5397
lahcen <lahcenwirghanec11@gmail.com>
2025-04-19 13:13:51 +0000

    connect to tmdb api and fetch popular movies

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,7 +6,7 @@ export default function MovieCard({movie}){
     return (
         <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg mb-6 transition-transform duration-300 hover:scale-105 hover:shadow-2xl">
             <div className="relative">
-                <img src={movie.url} alt={movie.title} className="w-full object-cover" />
+                <img src={`https://images.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="w-full object-cover" />
                 <div className="absolute top-2 right-2">
                     <button 
                         onClick={handleFavorite}
@@ -18,7 +18,7 @@ export default function MovieCard({movie}){
             </div>
             <div className="p-4">
                 <h3 className="text-xl font-semibold text-white mb-1">{movie.title}</h3>
-                <p className="text-gray-400 text-sm">{movie.release_date}</p>
+                <p className="text-gray-400 text-sm">{movie.release_date?.split('-')[0]}</p>
             </div>
             <hr className="border-gray-700 my-2"/>
         </div>
